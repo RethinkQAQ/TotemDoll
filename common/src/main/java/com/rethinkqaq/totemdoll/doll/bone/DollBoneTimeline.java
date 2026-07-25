013a452e9d2ac4c9c3ea47ba995c528ae216ec1a
Rethink_QAQ <Kathleen_Come@outlook.com>
2026-07-25 23:21:37 +0800

    Add LGPL headers and build workflow

--- common/src/main/java/com/rethinkqaq/totemdoll/doll/bone/DollBoneTimeline.java
+++ common/src/main/java/com/rethinkqaq/totemdoll/doll/bone/DollBoneTimeline.java
@@ -1,3 +1,23 @@
+/*
+ * Totem Doll
+ * Copyright (C) 2026 Rethink_QAQ
+ *
+ * This file is part of Totem Doll.
+ *
+ * Totem Doll is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Totem Doll is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Totem Doll. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.rethinkqaq.totemdoll.doll.bone;
 
 import java.util.List;
